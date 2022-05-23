a3e9ffab92ab2306b06e69183a97678c7ae64c55
Bradford Knight <bradford.a.knight@gmail.com>
2022-05-23 13:04:36 -0400

    Doing Activity

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// Function to add images
+function newImage(url, left, bottom){
+    let value = document.createElement('img')
+    value.src = url
+    value.style.position = 'fixed'
+    value.style.left = left + 'px'
+    value.style.bottom = bottom + 'px'
+    document.body.append(value)
+}
+
+
+// Item Function
+function newItem(url,left,bottom){
+    let value = newImage(url,left,bottom)
+
+    value.addEventListener('dblclick', () => {
+        value.remove()
+    })
+}
+// Add Green Character
+newImage('assets/green-character.gif',100,100)
+
+
+// Add Pine Tree
+newImage('assets/pine-tree.png',450,200)
+
+// Add Tree
+newImage('assets/tree.png',200,300)
+
+// Add Pillar
+newImage('assets/pillar.png',350,100)
+
+// Add Crate
+newImage('assets/crate.png',150,200)
+
+// Add Well
+newImage('assets/well.png',500,425)
+
+
+
+// Add Sword Item
+newItem('assets/sword.png',500,405)
+
+// Add Shield Item
+newItem('assets/sheild.png',165,185)
+
+// Add Staff Item
+newItem('assets/staff.png',600,100)
+
